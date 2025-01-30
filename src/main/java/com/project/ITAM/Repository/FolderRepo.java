727e412868302df282b41844c3dc7166b8527a9e
AssestRegister <gargbhupesh24@gmail.com>
2025-01-30 11:31:53 +0000

    changes in groups and folder

--- src/main/java/com/project/ITAM/Repository/FolderRepo.java
+++ src/main/java/com/project/ITAM/Repository/FolderRepo.java
@@ -14,14 +14,10 @@ public interface FolderRepo extends JpaRepository<Folder,Long> {
     // Find all root folders (parentFolder is NULL)
     List<Folder> findByParentFolderIsNull();
 
-    List<Folder> findByParentFolderId(Long parentId);
-
-    List<Folder> findByUserFolderId(Long userId);
-
     @Query("SELECT f FROM Folder f " +
             "LEFT JOIN f.allowedUsers u " +
             "WHERE( f.folderType = 'PUBLIC' " +
-            "   OR f.owner.id = :userId " +
+            "   OR f.user.id = :userId " +
             "   OR (f.folderType = 'RESTRICTED' AND u.id = :userId))")
     List<Folder> findAccessibleFolders(@Param("userId") Long userId);
 
