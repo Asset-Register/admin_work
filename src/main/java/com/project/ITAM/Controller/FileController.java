35fa25e88443710ed6c833ea803e57b132657134
AssestRegister <gargbhupesh24@gmail.com>
2025-02-01 06:11:36 +0000

    swagger changes

--- src/main/java/com/project/ITAM/Controller/FileController.java
+++ src/main/java/com/project/ITAM/Controller/FileController.java
@@ -3,15 +3,12 @@ package com.project.ITAM.Controller;
 import com.project.ITAM.Model.FileEntity;
 import com.project.ITAM.Model.FileEntityRequest;
 import com.project.ITAM.Service.FileService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Api(tags = "FileAPI")
 @RestController
 @RequestMapping("api/files")
 public class FileController {
@@ -19,7 +16,6 @@ public class FileController {
     @Autowired
     private FileService fileService;
 
-    @ApiOperation(value = "Get a message", notes = "Returns a simple hello message")
     @PostMapping("/upload")
     public ResponseEntity<FileEntity> uploadFile(@RequestBody FileEntityRequest fileEntityRequest) {
         return ResponseEntity.ok(fileService.uploadFile(fileEntityRequest));
