97cd18b52492f259fe9582af41fe679c96b661dc
AssestRegister <gargbhupesh24@gmail.com>
2025-02-11 07:31:49 +0000

    new security files added

---
+++ src/main/java/com/project/ITAM/Config/AzureADConfig.java
@@ -0,0 +1,40 @@
+
+package com.project.ITAM.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AzureADConfig {
+
+    @Value("${security.mode}")
+    private String clientId;
+
+    @Value("${security.mode}")
+    private String clientSecret;
+
+    @Value("${security.mode}")
+    private String authority;
+
+
+    private ClientRegistration azureAdClientRegistration() {
+        return ClientRegistration.withRegistrationId("azure")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("https://localhost:8082/login/oauth2/code/f1db1ed5-5658-49cf-956b-adcaa3b0f7e4")
+                .scope("openid", "profile", "email")
+                .authorizationUri(authority + "/oauth2/v2.0/authorize")
+                .tokenUri(authority + "/oauth2/v2.0/token")
+                .userInfoUri(authority + "/oauth2/v2.0/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .clientName("Azure AD")
+                .build();
+    }
+}
+
---
+++ src/main/java/com/project/ITAM/Config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.project.ITAM.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${security.mode}")
+    private String securityMode;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        switch (securityMode.toLowerCase()) {
+            case "sso":
+                configureSSOSecurity(http);
+                break;
+            case "basic":
+                configureBasicAuthSecurity(http);
+                break;
+            default:
+                configureNoSecurity(http);
+                break;
+        }
+        return http.build();
+    }
+
+    private void configureSSOSecurity(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll()  // Public endpoints
+                        .anyRequest().authenticated()  // Secure all other endpoints
+                ).oauth2Login();
+    }
+
+    private void configureBasicAuthSecurity(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll()  // Public endpoints
+                        .anyRequest().authenticated()  // Secure all other endpoints
+                )
+                .httpBasic(httpBasic -> {});
+    }
+
+    private void configureNoSecurity(HttpSecurity http) throws Exception {
+        http
+              //  .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                       // .requestMatchers("/api/auth/**").authenticated()  // Public endpoints
+                        .anyRequest().permitAll()
+                );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+  /*  public boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null && authentication.isAuthenticated();
+    }*/
+
+    /*@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }*/
+}
