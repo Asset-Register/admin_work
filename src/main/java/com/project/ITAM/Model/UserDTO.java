688d4ce077850595371420a7b665a56ce4fc222b
AssestRegister <gargbhupesh24@gmail.com>
2025-04-18 17:09:02 +0000

    changes in users and groups file

--- src/main/java/com/project/ITAM/Model/UserDTO.java
+++ src/main/java/com/project/ITAM/Model/UserDTO.java
@@ -1,14 +1,13 @@
 package com.project.ITAM.Model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity
 @Builder
 @Data
 @NoArgsConstructor
